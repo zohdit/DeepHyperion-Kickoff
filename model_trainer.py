f305a6855ff67ad94f06b646049b6a7552d2410e
zohdit <69303750+zohdit@users.noreply.github.com>
2020-09-11 10:18:46 +0200

    Update model_trainer.py

--- model_trainer.py
+++ model_trainer.py
@@ -5,7 +5,7 @@ Gets to 99.25% test accuracy after 12 epochs
 '''
 
 from __future__ import print_function
-import keras
+from tensorflow import keras
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
